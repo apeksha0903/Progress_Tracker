b5e73cb1b68edfbae41747017d928a261c56f506
AvaneeshLad <avaneeshlad2358@gmail.com>
2026-04-19 22:45:11 +0530

    Setup mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
 
+// connect database
+connectDB();
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
-  res.send("Backend is running...");
+  res.send("Backend + DB connected 🚀");
 });
 
 const PORT = process.env.PORT || 5000;
